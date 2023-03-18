b4e6f0aadd1f86c8c7091426f94131dab8336f29
aluno <Rodrigo Arajo>
2023-03-18 11:00:47 -0300

    Terceira Atualização

--- galeria/views.py
+++ galeria/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+
 
 def index (request):
-    return HttpResponse ('<h1> Senac no Espaço </h1><p> Bem vindo ao Espaço</p>')
+    return render(request, 'galeria/index.html')
 
+def imagem (request):
+    return render(request, 'galeria/imagem.html')
